c3276eb8d20eefc0c17f944d6e73147a3171e707
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-09-21 18:15:10 +0200

    added-table-in-index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>
@@ -11,7 +12,45 @@
         <h1>Comics</h1>
     </div>
     <div class="card-body">
-        @dump($comics)
+    <table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Thumb</th>
+      <th scope="col">Price</th>
+      <th scope="col">Series</th>
+      <th scope="col">Sale Date</th>
+      <th scope="col">type</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse($comics as $comic)
+      <tr>
+        
+        <td>{{$comic->title}}</td>
+        <td>{{$comic->description}}</td>
+        <th scope="row">
+            @if($comic->thumb)
+              <img src="{{$comic->thumb}}" alt="{{$comic->title}}" class="img-fluid" width="20"/>
+            @endif
+            {{$comic->title}}
+        </th>
+        <td>{{$comic->price}}</td>
+        <td>{{$comic->series}}</td>
+        <td>{{$comic->sale_date}}</td>
+        <td>{{$comic->type}}</td>
+      </tr>
+    @empty
+      <tr>
+        <td colspan = "8">
+            <h3>Nessun Fumetto disponibile</h3>
+        </td>
+      </tr>
+    @endforelse
+    
+</table>
     </div>
 </body>
 </html>
